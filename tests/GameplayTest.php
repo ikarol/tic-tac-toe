3d2cb60c70215c67120a272378d5151e9403913f
Ivan Karol <ikarol@gomel.iba.by>
2017-09-08 23:36:21 +0300

    Updated existing test for Gameplay

--- tests/GameplayTest.php
+++ tests/GameplayTest.php
@@ -1,11 +1,26 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use Tictactoe\Gameplay;
+use Tictactoe\HumanPlayer;
+use Tictactoe\ComputerPlayer;
 
 final class GameplayTest extends TestCase
 {
-    public function testOnClassInstantiation()
+    /**
+     * @test
+     */
+    public function on_human_wins()
     {
-        $game = new Gameplay();
+        $human = new HumanPlayer();
+        $computer = new ComputerPlayer();
+        $game = new Gameplay($human, $computer);
+        $this->assertEquals(true, $game->startGame());
+        $game->playerOneTurn(1, 1);
+        $this->assertEquals(false, $game->isCellEmpty(1, 1));
+        $game->playerTwoTurn(2, 1);
+        $game->playerOneTurn(1, 0);
+        $game->playerTwoTurn(2, 0);
+        $game->playerOneTurn(1, 2);
+        $this->assertEquals($human, $game->getWinner());
     }
 }
