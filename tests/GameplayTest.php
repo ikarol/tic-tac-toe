609c033b119af4e7d5a59627c2c4f0f79b66222f
Ivan Karol <ikarol@gomel.iba.by>
2017-09-08 19:06:25 +0300

    Created Gameplay class test case

---
+++ tests/GameplayTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class GameplayTest extends TestCase
+{
+    public function testOnClassInstantiation()
+    {
+        $game = new Gameplay();
+    }
+}
