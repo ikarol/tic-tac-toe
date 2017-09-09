cf386f093264b1768ea613dc2c8dcb1db65382e6
Ivan Karol <ikarol@gomel.iba.by>
2017-09-09 20:32:40 +0300

    Added draw test scenario for Gameplay

--- tests/GameplayTest.php
+++ tests/GameplayTest.php
@@ -5,6 +5,7 @@ use Tictactoe\HumanPlayer;
 use Tictactoe\ComputerPlayer;
 use Tictactoe\Exceptions\CellIsNotEmptyException;
 use Tictactoe\Exceptions\OutOfFieldException;
+use Tictactoe\Exceptions\DrawException;
 
 final class GameplayTest extends TestCase
 {
@@ -52,6 +53,27 @@ final class GameplayTest extends TestCase
         $this->assertEquals(true, $game->startGame());
         $this->expectException(OutOfFieldException::class);
         $game->playerOneTurn(3, 5);
+    }
+
+    /**
+     * @test
+     */
+    public function on_draw()
+    {
+        $human = new HumanPlayer();
+        $computer = new ComputerPlayer();
+        $game = new Gameplay($human, $computer);
+        $this->assertEquals(true, $game->startGame());
+        $game->playerOneTurn(0, 0);
+        $game->playerTwoTurn(0, 1);
+        $game->playerOneTurn(0, 2);
+        $game->playerTwoTurn(1, 0);
+        $game->playerOneTurn(1, 1);
+        $game->playerOneTurn(1, 2);
+        $game->playerTwoTurn(2, 1);
+        $game->playerOneTurn(2, 0);
+        $this->expectException(DrawException::class);
+        $game->playerTwoTurn(2, 2);
     }
 
 }
