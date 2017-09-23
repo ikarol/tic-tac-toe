67eff98f3a2c6bc56707a9dcdb2a35014d51a8ce
Ivan Karol <ikarol@gomel.iba.by>
2017-09-23 20:55:06 +0300

    Replaced lumen with zend-expressive

--- src/Tictactoe/Plugin/UI/Web/public/index.php
+++ src/Tictactoe/Plugin/UI/Web/public/index.php
@@ -1,28 +1,17 @@
 <?php
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Zend\Diactoros\Response\TextResponse;
+use Zend\Expressive\AppFactory;
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
+chdir(dirname(__DIR__));
+require '../../../../../vendor/autoload.php';
 
-$app = require __DIR__.'/../bootstrap/app.php';
+$app = AppFactory::create();
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+$app->get('/', function ($request, DelegateInterface $delegate) {
+    return new TextResponse('Hello, world!');
+});
 
+$app->pipeRoutingMiddleware();
+$app->pipeDispatchMiddleware();
 $app->run();
